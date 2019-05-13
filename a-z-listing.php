4da6f6e082959d5cd2d3939fe470e605ce872d6f
millnerr <millnerr@elms.edu>
2019-05-13 14:54:28 -0400

    Revert "Attached terms take #3"
    This reverts commit 225da9f43006046e36d5abe80c58f4c89e66df47.

--- a-z-listing.php
+++ a-z-listing.php
@@ -67,25 +67,22 @@ function fixAZListingScroll() {
 									$accred = ', ' .get_field("accred");
 								};
 
-								<li>
-<?php foreach($filters as $filter):
-	$filter_fields = get_field('department', $filter);
-	if( $filter_fields ):
-		$filter_fields_slugs = array_column($filter_fields, 'slug');
-        $data_filter_slugs = implode(', ', $filter_fields_slugs);
-	endif;
-	echo $filter;
-	echo $filters
-endforeach;
+								$terms = get_field('department');
 
+								if( $terms ){
+									foreach( $terms as $term ) {
+										get_term_link( $term );
+									};
+								};
 
+								?>
+								<li class="<?php echo $terms ; ?>">
 									<a href="<?php the_permalink(); ?>"><strong><?php echo $prefix .get_field("first_name" ). ' '. get_field("last_name" ) . $accred ; ?></strong></a>
 								<?php $prefix = ""; ?>
 								<?php $accred = ""; ?>
 									<?php the_excerpt(); ?>
 
-				<?php echo get_the_term_list( $post->ID, 'department', '<label>Department(s):</label> ', ', ' ); ?>
-
+								<?php echo get_the_term_list( $post->ID, 'department', '', ', ' ); ?>
 
 								</li>
 							<?php endwhile; ?>
