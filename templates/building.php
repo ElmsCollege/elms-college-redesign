9a25511b42750fd4332b470258029b4840aa8978
millnerr <millnerr@elms.edu>
2019-05-15 15:27:46 -0400

    Downloaded a new plugin (Frontend Gallery Slider For Advanced Custom Field) that allows us to have a slider on the page without backend coding. Removing the building_gallery field from the template because it is no longer needed

--- templates/building.php
+++ templates/building.php
@@ -26,20 +26,6 @@ get_header(); ?>
 
 		<div class="field-content">
 			<?php the_content() ?>
-
-			<?php
-				$images = get_field("building_gallery");
-				$size = "medium"; // (thumbnail, medium, large, full or custom size)
-
-				if( $images ): ?>
-					<ul>
-						<?php foreach( $images as $image ): ?>
-							<li>
-								<?php echo wp_get_attachment_image( $image['ID'], $size ); ?>
-							</li>
-						<?php endforeach; ?>
-					</ul>
-				<?php endif; ?>
 		</div>
 
 
