0990cc499d35ac1785261a21a04d293bf6c36b44
millnerr <millnerr@elms.edu>
2019-05-16 10:16:12 -0400

    Bringing back the slider nav bar
    Bringing back the slider nav bar because it looks weird without it.
    Also tweaking the slider JS

--- js/functions.js
+++ js/functions.js
@@ -530,33 +530,27 @@
       $(".hero-column:nth-child(4)").velocity({width: (25+err)+"%", left: (25+25+25-err)+"%"}, {easing: "easeInOutCirc", duration: duration});
     });
 
-	  //slick slider initialization
-	jQuery(".slider-for").slick({
-	  	slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: true,
-		fade: true,
-//		asNavFor: '.slider-nav',
-		lazyLoad: 'ondemand',
-		centerMode: true,
-		variableWidth: true,
-		infinite: true,
-		autoplay: true,
-	});
-/*
-	jQuery(".slider-nav").slick({
+	//slick slider initialization
+	$('.slider-for').slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.slider-nav'
+		lazyLoad: 'ondemand',
+		infinite: true
+	});
+	$('.slider-nav').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
 		asNavFor: '.slider-for',
-		dots: false,
+ 		dots: true,
 		centerMode: true,
 		focusOnSelect: true,
-		arrows: true,
-		autoplay: false,
-		lazyLoad: 'ondemand',
+		infinite: true,
 	});
-*/
-  });
+
+  });//end document.ready
   
 	$('a[href*="#program-track-anchor"]').click(function(e){
 		e.preventDefault();
