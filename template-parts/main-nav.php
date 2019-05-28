c44639365288413a620d8026e7ce3a8e8cec6969
millnerr <millnerr@elms.edu>
2019-05-28 12:06:56 -0400

    Adding search icon back in
    When merging content between trunk and this branch, the icons for the
    search bar were lost. Adding those back in and making the html human
    readable

--- template-parts/main-nav.php
+++ template-parts/main-nav.php
@@ -6,21 +6,40 @@
   <div class="close-label">Close</div>
 </div>
 <nav id="site-navigation" class="main-navigation" role="navigation">
-  
-  <?php /*<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'gs_elms' ); ?></button> */ ?>
 
-<div class="menu-special-menu-container"><ul id="special-menu" class="menu"><li id="menu-item-2462" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2462 search-item"><a href="https://elms-staging.r6a5yukd-liquidwebsites.com/search/"><span class="search-realtext">Search</span><span class="search-icon"></span></a><ul class="sub-menu for-search"><li id="menu-item-search" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-search"><div id="menu-search-dropdown" style="">
-  <form role="search" method="get" class="search-form" action="/?">
-	<label>
-		<span class="screen-reader-text">Search for:</span>
-		<input type="search" class="search-field" placeholder="What are you looking for?" value="" name="s" title="Search for:">
-	</label>
-	<input type="submit" class="search-submit" value="Search">
-</form></div></li></ul></li>
-<li id="menu-item-9764" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9764"><a id="interested-in-applying" href="https://www.elms.edu/interested-in-applying/" style="">Apply</a><a id="link-give" href="https://www.elms.edu/alumni/support-elms/make-a-gift/" style="">Give</a></li>
-</ul></div>
+<div class="menu-special-menu-container">
+	<ul id="special-menu" class="menu">
+		<li class="menu-item menu-item-type-post_type menu-item-object-page search-item">
+			<a href="/search/">
+				<span class="search-realtext">Search</span>
+				<span class="search-icon">
+					<i class="fas fa-search" aria-hidden="true"></i>
+				</span>
+			</a>
+			<ul class="sub-menu for-search">
+				<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-search">
+					<div id="menu-search-dropdown">
+						<form role="search" method="get" class="search-form" action="/?">
+							<label>
+								<span class="screen-reader-text">Search for:</span>
+								<input type="search" class="search-field" placeholder="What are you looking for?" value="" name="s" title="Search for:">
+							</label>
+							<button type="submit" class="search-submit" value="Search" aria-label="Search form submit button">
+								<i class="fas fa-search" aria-hidden="true"></i>
+							</button>
+						</form>
+					</div>
+				</li>
+			</ul>
+		</li>
+		<li id="menu-item-9764" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9764">
+			<a id="interested-in-applying" href="https://www.elms.edu/interested-in-applying/">Apply</a>
+			<a id="link-give" href="https://www.elms.edu/alumni/support-elms/make-a-gift/">Give</a>
+		</li>
+	</ul>
+</div>
 
-	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
   
 </nav><!-- #site-navigation -->
 <div id="menu-search-dropdown" style="display:none;">
