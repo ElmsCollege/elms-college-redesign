3c160ce9fe127dc39c3d415e9eaba722245cdad7
millnerr <millnerr@elms.edu>
2019-05-28 12:30:29 -0400

    Revert "Trying to clean up the nav bar when JS is off"
    This reverts commit c70390308898036c6606901df0e7551565f9a210.

--- template-parts/main-nav.php
+++ template-parts/main-nav.php
@@ -7,8 +7,6 @@
 </div>
 <nav id="site-navigation" class="main-navigation" role="navigation">
 
-<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-  
 <div class="menu-special-menu-container">
 	<ul id="special-menu" class="menu">
 		<li class="menu-item menu-item-type-post_type menu-item-object-page search-item">
@@ -34,13 +32,15 @@
 				</li>
 			</ul>
 		</li>
-		<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9764">
+		<li id="menu-item-9764" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9764">
 			<a id="interested-in-applying" href="https://www.elms.edu/interested-in-applying/">Apply</a>
 			<a id="link-give" href="https://www.elms.edu/alumni/support-elms/make-a-gift/">Give</a>
 		</li>
 	</ul>
 </div>
 
+<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+  
 </nav><!-- #site-navigation -->
 <div id="menu-search-dropdown" style="display:none;">
   <?php get_search_form() ?>
