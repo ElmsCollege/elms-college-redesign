b1146222cdc062d04b54502d91b1eec17e02dd02
millnerr <millnerr@elms.edu>
2019-05-29 10:57:45 -0400

    Fixing some display issues discovered while browsing testing this branch

--- template-parts/main-nav.php
+++ template-parts/main-nav.php
@@ -32,7 +32,7 @@
 				</li>
 			</ul>
 		</li>
-		<li id="menu-item-9764" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9764">
+		<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9764">
 			<a id="interested-in-applying" href="https://www.elms.edu/interested-in-applying/">Apply</a>
 			<a id="link-give" href="https://www.elms.edu/alumni/support-elms/make-a-gift/">Give</a>
 		</li>
