0c191ae975d88e3ea7161a9e748232ec525105dc
millnerr <32333237+millnerr@users.noreply.github.com>
2019-06-25 12:10:54 -0400

    Previous commit worked, styling the LI that displays the language name and the flag

--- template-parts/page-heading.php
+++ template-parts/page-heading.php
@@ -8,6 +8,9 @@ if( get_field('featured_image_cropped') ){
 	echo '<div id="textHeading" class="section-heading">';
 }
 ?>
+<style>
+	.languageSwitcher a,.languageSwitcher a:visited{color:white;}
+</style>
 	<h1 class="field-title">
 		<?php 
 			echo esc_html( get_the_title() );
@@ -15,7 +18,7 @@ if( get_field('featured_image_cropped') ){
 	</h1>
 	<?php 
 	  if( function_exists("pll_the_languages")){
-		echo '<ul class="ulreset" style="align-self:end;margin-left:5px;text-align:center;color:white;">';
+		echo '<ul class="languageSwitcher ulreset" style="align-self:end;margin-left:5px;text-align:center;color:white;">';
 	  	pll_the_languages( array( 'show_flags' => 1,'hide_current' => 1,'hide_if_no_translation' => 1) );
 	  	echo '</ul>';
 	  }
