db1d5f0eb52018c7310c03d037811fb96bdba295
millnerr <32333237+millnerr@users.noreply.github.com>
2019-06-26 14:32:11 -0400

    The styling for the library-specific and nursing-specific nav bars are identical but we have to declare all the rules twice to style them, this seems silly. Trying to give each each special nav bar the same id (#special-section-menu) so we can use the same rules for each

--- header-library.php
+++ header-library.php
@@ -53,5 +53,5 @@ add_filter( 'body_class', function( $classes ) {
 	<div id="content" class="site-content">
     <?php get_template_part("template-parts/alert-bar")?>
     <div class="section-library-nav">
-      <?php wp_nav_menu( array( 'theme_location' => 'library', 'menu_id' => 'library-menu'/*, 'depth' => 1*/ ) ); ?>
+      <?php wp_nav_menu( array( 'theme_location' => 'library', 'menu_id' => 'special-section-menu', 'menu_class' => 'ulreset'/*, 'depth' => 1*/ ) ); ?>
     </div>
--- header-nursing.php
+++ header-nursing.php
@@ -53,5 +53,5 @@ add_filter( 'body_class', function( $classes ) {
 	<div id="content" class="site-content">
     <?php get_template_part("template-parts/alert-bar")?>
     <div class="section-nursing-nav">
-      <?php wp_nav_menu( array( 'theme_location' => 'nursing', 'menu_id' => 'nursing-menu'/*, 'depth' => 1*/ ) ); ?>
+      <?php wp_nav_menu( array( 'theme_location' => 'nursing', 'menu_id' => 'special-section-menu', 'menu_class' => 'ulreset'/*, 'depth' => 1*/ ) ); ?>
     </div>
