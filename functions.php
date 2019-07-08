d9a45a2964a7a544e0a27b0109a469a40c27496b
millnerr <32333237+millnerr@users.noreply.github.com>
2019-07-08 15:23:31 -0400

    Adding published date and reducing event image size
    index.php - removing the RSS link and adding a "Published on" field to let users know the content is relevant.
    functions.php - now that the smaller images are being used for the homepage events, we can use the "medium" image size instead of the large

--- functions.php
+++ functions.php
@@ -283,7 +283,7 @@ add_action('wp_head', 'gs_add_typekit');
 function render_homepage_event ($event) {
   ?> 
   <div class="event">
-    <div class="event-image" style="<?php print_featured_image_style($event->ID, "large") ?>">
+    <div class="event-image" style="<?php print_featured_image_style($event->ID, "medium") ?>">
       <!-- <?php print get_the_post_thumbnail($event->ID) ?> -->
       
       <div class="start-date">
