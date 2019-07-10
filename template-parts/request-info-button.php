f07c97e70dc267b28005a139b2966104af127a61
millnerr <32333237+millnerr@users.noreply.github.com>
2019-07-10 11:45:28 -0400

    Running out of time to get the show/hide working. Reverting those changes for the time being

--- template-parts/request-info-button.php
+++ template-parts/request-info-button.php
@@ -1,5 +1,5 @@
 <?php 
-	if (get_field('control_request_info_button') != 'hide'):
+//	if (get_field('control_request_info_button') != 'hide'):
 		echo do_shortcode("[sc name='get-info-popup']");
-	endif;
+//	endif;
 ?>
