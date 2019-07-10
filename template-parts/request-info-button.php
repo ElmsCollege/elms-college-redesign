c23b7ac70080cfd5693bb3a2f0dd9733cbe241d2
millnerr <32333237+millnerr@users.noreply.github.com>
2019-07-10 11:28:27 -0400

    Removing quotes from the showRequestInfo variable because PHP might be setting it to be a string instead of an integer.

--- template-parts/request-info-button.php
+++ template-parts/request-info-button.php
@@ -1,5 +1,5 @@
 <?php 
-	$showRequestInfo = "1";
+	$showRequestInfo = 1;
 	$showRequestInfo = get_field('control_request_info_button');			
 	if( $showRequestInfo == 1 ): 
 		echo do_shortcode("[sc name='get-info-popup']");
