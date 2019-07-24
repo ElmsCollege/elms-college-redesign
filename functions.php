2f45fde78cc7fc46d9ee6026b3d46847c445d5b2
millnerr <32333237+millnerr@users.noreply.github.com>
2019-07-24 09:03:14 -0400

    Reverting changes to the code that creates the events section of the homepage because the entire idea while have to be rethought

--- functions.php
+++ functions.php
@@ -292,11 +292,8 @@ add_action('wp_head', 'gs_add_typekit');
 function render_homepage_event ($event) {
   ?> 
   <div class="event">
-    <div class="event-image" style="<?php //print_featured_image_style($event->ID, "thumbnail") ?>">
-      <?php 
-		//print get_the_post_thumbnail($event->ID) 
-		echo get_field("featured_image_cropped", $post_id, "thumbnail")
-		?>
+    <div class="event-image" style="<?php print_featured_image_style($event->ID, "medium") ?>">
+      <?php print get_the_post_thumbnail($event->ID) ?>
       
       <div class="start-date">
         <div class="day">
