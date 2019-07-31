7473a532331a8ad1a9e767cb4ba40e0cdafe8f4d
millnerr <32333237+millnerr@users.noreply.github.com>
2019-07-31 09:25:42 -0400

    Proper PHP take three - quote position matters

--- functions.php
+++ functions.php
@@ -294,7 +294,7 @@ function display_homepage_event ($event) {
 			if ( has_post_thumbnail() ) {
 				print get_the_post_thumbnail($event->ID, "medium", array( "class" => "grey-to-color" ));
 			}else{
-				echo "<img class='grey-to-color' src='/wp-content/uploads/2018/10/Elms-Campus_aerial-view-300x169.jpg' alt='Photo of Elms College campus'>;"
+				echo "<img class='grey-to-color' src='/wp-content/uploads/2018/10/Elms-Campus_aerial-view-300x169.jpg' alt='Photo of Elms College campus'>";
 			};
 		?>
     </div>
