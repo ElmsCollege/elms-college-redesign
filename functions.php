474fe86a9a1e524b74c63c3893bf4f0797fcfa5e
millnerr <32333237+millnerr@users.noreply.github.com>
2019-07-31 09:02:46 -0400

    Tweaking the function that displays the events on the homepage. The goal is to have a default image if there isn't a featured image attached to the event.

--- functions.php
+++ functions.php
@@ -289,8 +289,15 @@ add_action('wp_head', 'gs_add_typekit');
 function display_homepage_event ($event) {
   ?> 
   <a class="event" href="<?php print get_the_permalink($event) ?>">
-    <div class="event-image" style="<?php //print_featured_image_style($event->ID, "medium") ?>">
-      <?php print get_the_post_thumbnail($event->ID, "medium", array( "class" => "grey-to-color" )) ?>
+    <div class="event-image">
+		<?php
+			if ( has_post_thumbnail() ) {
+				print get_the_post_thumbnail($event->ID, "medium", array( "class" => "grey-to-color" ));
+			}else{
+				print "<img class='grey-to-color' src='/wp-content/uploads/2018/10/Elms-Campus_aerial-view-300x169.jpg' alt='Photo of Elms College campus'>";
+			}
+		?>
+      <?php  ?>
     </div>
     
     <div class="main-event-content">
