b2c1caf985cbd8ea91b22fb633d9a91b28f3e012
millnerr <32333237+millnerr@users.noreply.github.com>
2019-08-08 11:55:20 -0400

    Attempting to conditionally load a header specific for commencement that will allow us to have a special header on the commencement page like the one on the library and school of nursing pages.

---
+++ header-commencement.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The header for the nursing pages.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Elms_College_Redesign
+ */
+
+add_filter( 'body_class', function( $classes ) {
+	return array_merge( $classes, array( 'mobile-or-library' ) );
+});
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+
+<?php wp_head(); ?>
+<?php the_field("head_code", "option") ?>
+</head>
+
+<body id="commencementPage" <?php body_class(); ?>>
+<?php the_field("body_code", "option") ?>
+
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'gs_elms' ); ?></a>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="site-branding">
+			<?php
+			if ( is_front_page() && is_home() ) : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( real_homepage_link() ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php else : ?>
+				<p class="site-title"><a href="https://www.elms.edu/" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php
+			endif;
+
+			$description = get_bloginfo( 'description', 'display' );
+			if ( $description || is_customize_preview() ) : ?>
+				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+			<?php
+			endif; ?>
+		</div><!-- .site-branding -->
+
+		<?php get_template_part("template-parts/main-nav")?>
+	</header><!-- #masthead -->
+  
+	<div id="content" class="site-content">
+    <?php get_template_part("template-parts/alert-bar")?>
+    <div class="section-nursing-nav">
+      <?php wp_nav_menu( array( 'theme_location' => 'main', 'menu_id' => 'special-section-menu', 'menu_class' => 'ulreset'/*, 'depth' => 1*/ ) ); ?>
+    </div>
