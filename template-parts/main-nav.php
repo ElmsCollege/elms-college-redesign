ebd3ca1223f96ae059907562265b4b03033cb515
millnerr <32333237+millnerr@users.noreply.github.com>
2019-08-08 14:50:28 -0400

    Commenting out the nav bar copy menu function to see if that is what is breaking the pagelayout

--- template-parts/main-nav.php
+++ template-parts/main-nav.php
@@ -43,6 +43,7 @@
 /* We are switching to global blog menu for local,
  * it will be easy for admin to add or delete menu which apply across to the site
  */
+	/*
 global $blog_id;
 $current_blog_id = $blog_id;
 switch_to_blog(1);
@@ -52,6 +53,7 @@ switch_to_blog(1);
     ) );
 
 switch_to_blog($current_blog_id); 
+*/
 ?>
   
 </nav><!-- #site-navigation -->
