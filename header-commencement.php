6862f45781e3ca69b6e49727f0cf983c9eb4e867
millnerr <32333237+millnerr@users.noreply.github.com>
2019-08-14 13:53:22 -0400

    Quick attempt to use flexbox to style the header - anecdotal evidence says that this might break the nav bar button

--- header-commencement.php
+++ header-commencement.php
@@ -31,25 +31,26 @@ add_filter( 'body_class', function( $classes ) {
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'gs_elms' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( real_homepage_link() ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title">
-					<a href="https://www.elms.edu" rel="home" alt="Return to homepage">Elms College</a>
-					| <a href="https://commencement.elms.edu">Commencement</a>
-				</p>
-			<?php
-			endif;
+		<div style="margin-right:auto;">
+			<div class="site-branding">
+				<?php
+				if ( is_front_page() && is_home() ) : ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( real_homepage_link() ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php else : ?>
+					<p class="site-title">
+						<a href="https://www.elms.edu" rel="home" alt="Return to homepage">Elms College</a>
+					</p>
+				<?php
+				endif;
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-		<div class="specialsectionlink"><a href="https://commencement.elms.edu/" title="Return to Commencement Homepage">Commencement</a></div>
+				$description = get_bloginfo( 'description', 'display' );
+				if ( $description || is_customize_preview() ) : ?>
+					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+				<?php
+				endif; ?>
+			</div><!-- .site-branding -->
+			<div class="specialsectionlink"><a href="https://commencement.elms.edu/" title="Return to Commencement Homepage">Commencement</a></div>
+		</div>
 		<?php get_template_part("template-parts/main-nav")?>
 	</header><!-- #masthead -->
   
