6892081cab47df831eece3ed861651f98716652e
millnerr <32333237+millnerr@users.noreply.github.com>
2019-08-29 09:06:17 -0400

    Delay problem is selector based
    The "things will only close" issue isn't because of a the JS its because of the selector. When a user clicks the option, the JS is getting the id of the building (berchmans, library) and doing the correct thing but then it is also getting the id of the all-encompassing <g> element (campusMap) and closing things again. Trying to remove the all-encompassing <g> element to see if that works with the zoom.

--- templates/campus-map.php
+++ templates/campus-map.php
@@ -23,8 +23,8 @@ get_header(); ?>
 	<main id="main" class="site-main pure-u-1" role="main">
 
 		<div id="svg-container" class="flexRowWrapStart spaceBetween">
-<svg id="Layer_1" viewBox="0 0 10000 4500" xml:space="preserve" preserveAspectRatio="xMidYMid meet" width="1280" height="576" >
-	<g id="campusMap">
+<svg id="campusMap" viewBox="0 0 10000 4500" xml:space="preserve" preserveAspectRatio="xMidYMid meet" width="1280" height="576" >
+	<!--<g id=""> -->
 <path class="st0" d="M1282.5,1626c0-0.1,0-0.2,0-0.2c0,0,0-0.1,0-0.1c0-0.1,0-0.1-0.1-0.2c0,0,0-0.1-0.1-0.1c0,0,0,0,0-0.1
 	c0,0-0.1-0.1-0.1-0.1c0,0,0-0.1-0.1-0.1c-0.1-0.1-0.1-0.1-0.2-0.2c0,0,0,0,0,0l0,0c0,0,0,0,0,0l-170-123.1c-0.5-0.4-1.2-0.4-1.7-0.1
 	l-63.6,40l-28.9-21l84.9-53.4c0.2,0.2,0.5,0.3,0.8,0.3c0.8,0,1.5-0.7,1.5-1.5v-1c0,0,0,0,0-0.1c0-0.1,0-0.2,0-0.2c0,0,0-0.1,0-0.1
@@ -412,7 +412,7 @@ get_header(); ?>
 	c0,0,0.1,0,0.1-0.1c0.1-0.1,0.1-0.1,0.2-0.2c0,0,0.1-0.1,0.1-0.1c0,0,0,0,0-0.1c0.1-0.1,0.1-0.2,0.2-0.3c0,0,0,0,0,0l2-4.7
 	c0-0.1,0-0.1,0.1-0.2l1.9-6.4c0-0.1,0-0.1,0-0.2l1-6.4c0-0.1,0-0.1,0-0.2l0.2-6.5v-1C5606.9,2801.3,5606.9,2801.3,5606.9,2801.2z">
 	<title>Keating Quadrangle</title></path>
-	</g>
+	<!--</g>-->
 </svg>
 			<div id="index" class="js-accordion">
 				<div class="berchmans js-accordion__panel">
@@ -444,8 +444,6 @@ get_header(); ?>
 			jQuery('.js-accordion__panel').attr({
 				"aria-hidden":"true"
 			});
-			console.log("trying to increase the time between closing the panels and opening the new ones");
-			jQuery(this).delay(200);
 			var buildingId = this.id;
 			console.log('building id= ' + buildingId);
 			var accordionPanel = jQuery('.' +buildingId + '.js-accordion__panel');
