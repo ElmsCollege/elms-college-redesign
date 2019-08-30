533c72fc6a2c54334a27c8ce40ef513ec87c708b
millnerr <32333237+millnerr@users.noreply.github.com>
2019-08-30 14:36:03 -0400

    General maintenance
    Removing an image that doesn't exist from the stylesheet.
    
    Cleaning up some comments in the content-type templates to make their purpose more clear.

--- single-student_story.php
+++ single-student_story.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
+ * This template controls how the student stories are displayed
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
