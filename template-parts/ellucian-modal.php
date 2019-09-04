54356b2a72304a02a5a40868086a5b03ebaed544
millnerr <32333237+millnerr@users.noreply.github.com>
2019-09-04 09:22:14 -0400

    Still don't have the close button quite right

--- template-parts/ellucian-modal.php
+++ template-parts/ellucian-modal.php
@@ -8,7 +8,7 @@
 			</header>
 			<iframe src="https://elms.elluciancrmrecruit.com/Apply/Account/ProspectInquiryWidget?f=5dc21e7b-aa4f-4979-a72b-47f4a48c9901&o=0428151b-96a8-4fc4-912f-850a80b3c16a&s=24525250-9880-e811-80db-0a4fbe36cd62" height="350"></iframe>
 			<footer class="modal_footer">
-				<button class="requestInfo-close-trigger" aria-label="Close" data-micromodal-close>Close</button>
+				<button aria-label="Close" data-micromodal-close></button>
 			</footer>
 		</div>
 	</div>
