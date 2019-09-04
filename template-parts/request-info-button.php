4dd3102bc947e78f4ff125085b66eb7cbc88bb58
millnerr <32333237+millnerr@users.noreply.github.com>
2019-09-04 11:42:56 -0400

    Redid the show/hide request info button logic to take other factors into account. New plan:
    if the ACF value is set to 'show' then it will be shown
    if the ACF value is set to 'hide' then it will be hidden
    if the ACF value doesn't exist (isn't set) then the code looks at the URL and hides it if the URL contains 'about-elms', 'student-life', or 'alumni'

--- template-parts/request-info-button.php
+++ template-parts/request-info-button.php
@@ -1,5 +1,22 @@
-<?php 
-	if (get_field('control_request_info_button') != 'hide'):
-		echo do_shortcode("[sc name='get-info-popup']");
-	endif;
+<?php
+	$permalink = get_permalink();
+	if (get_field('control_request_info_button') == 'show'){
+		$showRequestInfo = true;		
+	} else if (get_field('control_request_info_button') == 'hide'){
+		$showRequestInfo = false;
+	} else {
+		if(strpos($permalink,'/about-elms/')){
+			$showRequestInfo = false;
+		} else if(strpos($permalink,'/alumni/')){
+			$showRequestInfo = false;
+		} else if(strpos($permalink,'/student-life/')){
+			$showRequestInfo = false;
+		} else{
+			$showRequestInfo = true;
+		}	
+	}
+
+	if($showRequestInfo === true){
+		echo do_shortcode("[sc name='get-info-popup']");		
+	}
 ?>
