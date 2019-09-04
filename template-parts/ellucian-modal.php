0060ad5aa621771e4a44afe58317fabb038fdd16
millnerr <32333237+millnerr@users.noreply.github.com>
2019-09-04 10:29:51 -0400

    Weird GitHub glitch. I hope this works.

--- template-parts/ellucian-modal.php
+++ template-parts/ellucian-modal.php
@@ -8,7 +8,7 @@
 			</header>
 			<iframe src="https://elms.elluciancrmrecruit.com/Apply/Account/ProspectInquiryWidget?f=5dc21e7b-aa4f-4979-a72b-47f4a48c9901&o=0428151b-96a8-4fc4-912f-850a80b3c16a&s=24525250-9880-e811-80db-0a4fbe36cd62" height="350"></iframe>
 			<footer class="modal_footer">
-				<button class="requestInfo-close-trigger" aria-label="Close" data-micromodal-close="data-micromodal-close">Close</button>
+				<a href="#" class="requestInfo-close-trigger" aria-label="Close this dialog" data-micromodal-close>Close</a>
 			</footer>
 		</div>
 	</div>
