90aafc51ae774c025356567230554b5737051f4e
millnerr <32333237+millnerr@users.noreply.github.com>
2019-09-04 12:26:03 -0400

    Updating micromodal.min.js to see if that fixes the console errors

--- functions.php
+++ functions.php
@@ -107,7 +107,7 @@ add_action( 'widgets_init', 'gs_elms_widgets_init' );
  */
 function gs_elms_scripts() {
 	wp_enqueue_style( 'wp-block-library-css', '/wp-includes/css/dist/block-library/style.min.css', array() );
-	wp_enqueue_style( 'micromodal', get_template_directory_uri() . '/css/micromodal.css', array(), '20190701');
+	wp_enqueue_style( 'micromodal', get_template_directory_uri() . '/css/micromodal.css', array(), '20190904');
 	
 	wp_enqueue_style( 'gs_elms-style', get_stylesheet_uri(), array(), '40' );
 
