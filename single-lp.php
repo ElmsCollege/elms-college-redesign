ed3621ebffe00976e38e710913b42f065cbea7d8
millnerr <32333237+millnerr@users.noreply.github.com>
2019-09-17 14:32:56 -0400

    Removed an erroneous duplicated do_shortcode that might have been causing trouble. Added a number to the shortcode trigger to ensure I'm looking at the latest version of the code

--- single-lp.php
+++ single-lp.php
@@ -47,9 +47,8 @@ get_header("advertising");
 						<a href="<?php the_field('advertising_application_link_url'); ?>" target="_blank" rel="noopener"><?php the_field('advertising_application_link_text'); ?></a>
 					</div>
 				<?php endif; ?>
-				<?php echo do_shortcode("[expand title='Get Information' id='formTrigger' swaptitle=' Close']" . get_template_part("template-parts/ellucian-modal") . "[/expand]"); ?>
+				<?php echo do_shortcode( '[expand title="Get Information 2" id="formTrigger" swaptitle= "Close"]' . locate_template( "ellucian-modal.php", true, true ) . '[/expand]' ); ?>
 				</div>
-<?php echo do_shortcode( '[expand title="Get Information" id="formTrigger" swaptitle= "Close"]' . locate_template( "ellucian-modal.php", true, true ) . '[/expand]' ); ?>
 			</div>
 		</div>
 	</main><!-- #main -->    
