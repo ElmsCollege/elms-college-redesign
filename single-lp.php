5fba4d4e516beb6a311b8907ca71a1eff398e507
millnerr <32333237+millnerr@users.noreply.github.com>
2019-09-17 14:49:33 -0400

    Trying to create my own expandable item.
    Using the collapse-o-matic roll your own functionality to create a collapsed element to hold the

--- single-lp.php
+++ single-lp.php
@@ -37,17 +37,21 @@ get_header("advertising");
 				<?php the_content(); ?>
 			</div>
 
-			<div id="get-started" class="flex-column">
-				<!-- right side text box and ninja form  -->
-				<h2><?php echo $rightHeader; ?></h2>
-				<?php the_field("advertising_right_column_text"); ?>
+				<div id="get-started" class="flex-column">
+					<!-- right side text box and form  -->
+					<h2><?php echo $rightHeader; ?></h2>
+					<?php the_field("advertising_right_column_text"); ?>
 
-				<?php if( get_field("advertising_application_link_url") ): ?>
-					<div class="buttons blue">
-						<a href="<?php the_field('advertising_application_link_url'); ?>" target="_blank" rel="noopener"><?php the_field('advertising_application_link_text'); ?></a>
+					<?php if( get_field("advertising_application_link_url") ): ?>
+						<div class="buttons blue">
+							<a href="<?php the_field('advertising_application_link_url'); ?>" target="_blank" rel="noopener"><?php the_field('advertising_application_link_text'); ?></a>
+						</div>
+					<?php endif; ?>
+					<span class="collapseomatic" title="Get Info" id="form" >Get Info</span>
+					<span id="swap-form" style="display: none;">Close form</span>
+					<div id="target-form" class="collapseomatic_content">
+						<?php get_template_part("template-parts/ellucian-modal"); ?>
 					</div>
-				<?php endif; ?>
-				<?php echo do_shortcode( '[expand title="Get Information 2" id="formTrigger" swaptitle= "Close"]' . locate_template( "ellucian-modal.php", true, true ) . '[/expand]' ); ?>
 				</div>
 			</div>
 		</div>
