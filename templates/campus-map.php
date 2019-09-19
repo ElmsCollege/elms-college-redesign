004a23b85fe238bec584912533bb79f941fbaf90
millnerr <32333237+millnerr@users.noreply.github.com>
2019-09-19 16:08:08 -0400

    Minor change to my if(exists) logic because the first iteration didnt work

--- templates/campus-map.php
+++ templates/campus-map.php
@@ -51,7 +51,7 @@ get_header(); ?>
 		jQuery('svg g.cls-2 polygon,svg g.cls-2 path').on('click touch',function(){
 			hideSlideshow();
 			var buildingId = this.id;
-			if(buildingId.length){
+			if(buildingId.length > 0){
 				var slideshowBlock = jQuery('.' +buildingId + '.slideshow');
 				jQuery(slideshowBlock).toggleClass('visible');
 				jQuery(this).addClass('hilite');
