24550f789ce494672eba6e6fb58fb98310a4494c
millnerr <32333237+millnerr@users.noreply.github.com>
2019-09-30 15:53:13 -0400

    Adding the class has promise but I think the class is on the wrong element. Changing the target from the <svg> to #svg-container

--- templates/campus-map.php
+++ templates/campus-map.php
@@ -24,13 +24,13 @@ get_header();
 			function hideSlideshow(){
 				jQuery('svg g').removeClass('hilite');
 				jQuery('.slideshow').removeClass('visible');
-				jQuery('svg').removeClass('slideshowOpen');
+				jQuery('#svg-container').removeClass('slideshowOpen');
 			}
 			jQuery('svg polygon,svg path').on('click touch',function(){
 				var buildingId = this.id;
 				hideSlideshow();
 				if(buildingId){
-					jQuery('svg').addClass('slideshowOpen');
+					jQuery('#svg-container').addClass('slideshowOpen');
 					var slideshowBlock = jQuery('.' +buildingId + '.slideshow');
 					jQuery(slideshowBlock).toggleClass('visible');
 					jQuery(this).addClass('hilite');
