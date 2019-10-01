1c06cd98e94e8cd7b609c00b27a482e521b18261
millnerr <32333237+millnerr@users.noreply.github.com>
2019-10-01 13:43:53 -0400

    Ensuring that the open function works as a listener by commenting out the close function

--- templates/campus-map.php
+++ templates/campus-map.php
@@ -20,10 +20,10 @@ get_header();
 			var instance = new SVGPanZoom(document.getElementById('campusMap'),{
 				eventMagnet: document.getElementById('svg-container')
 			});
-document.getElementById("campusMap").addEventListener("click", (e) => {
-	console.log("clicked. should close slideshow");
-				hideSlideshow();
-});
+//document.getElementById("campusMap").addEventListener("click", (e) => {
+	//console.log("clicked. should close slideshow");
+		//		hideSlideshow();
+//});
 		
 			function hideSlideshow(){
 				jQuery('.slideshow').removeClass('visible');
