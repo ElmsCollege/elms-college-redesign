1a088a64d72dac32c212d903ad7f3929bb1794d4
millnerr <32333237+millnerr@users.noreply.github.com>
2019-10-08 14:23:13 -0400

    Starting the redesign by trying to make the header easier to work with. This is being done by refactoring the code to be less reliant on absolute positioning

--- template-parts/main-nav.php
+++ template-parts/main-nav.php
@@ -11,10 +11,10 @@
 	<ul id="special-menu" class="menu">
 		<li class="menu-item menu-item-type-post_type menu-item-object-page search-item">
 			<a href="/search/">
-				<span class="search-realtext">Search</span>
 				<span class="search-icon">
 					<i class="fas fa-search" aria-hidden="true"></i>
 				</span>
+				<span class="search-realtext">Search</span>
 			</a>
 			<ul class="sub-menu for-search">
 				<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-search">
@@ -33,7 +33,7 @@
 			</ul>
 		</li>
 		<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9764">
-			<a id="interested-in-applying" href="https://www.elms.edu/interested-in-applying/">Apply</a>
+			<a id="interested-in-applying" href="https://elms.elluciancrmrecruit.com/Apply/Account/Login">Apply</a>
 			<a id="link-give" href="https://www.elms.edu/alumni/support-elms/make-a-gift/">Give</a>
 		</li>
 	</ul>
