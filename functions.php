57cc494a1e69175376d06e17ce3e9a3c8794d905
millnerr <32333237+millnerr@users.noreply.github.com>
2019-11-20 11:25:55 -0500

    Adding content and placeholder back into the array. Also adding an empty paragraph block to the first column to test that iteration

--- functions.php
+++ functions.php
@@ -544,10 +544,12 @@ function cptui_register_my_cpts_lp() {
     'template' => array(
       array( 'core/image', array() ),
       array( 'advgb/columns', array('gutter' => '20'), array(
-        array( 'advgb/column', array(), array() ),
         array( 'advgb/column', array(), array(
-          array( 'core/heading', array('level' => '3','className' => 'noMarginTop') ),
           array( 'core/paragraph', array() ),
+		) ),
+        array( 'advgb/column', array(), array(
+          array( 'core/heading', array('level' => '3','className' => 'noMarginTop'),'placeholder' => 'Classes start...' ),
+          array( 'core/paragraph', array('content' => 'Applying to Elms College is easier than you think, and there’s no fee to apply.') ),
           array( 'core/block', array('ref' => 39731) ),
         ) ),
       ) )
