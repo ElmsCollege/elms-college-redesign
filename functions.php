787d5a7a3eafb520a40326ffa661f96a7802eef0
millnerr <32333237+millnerr@users.noreply.github.com>
2019-11-20 15:51:12 -0500

    Reverting previous changes because it looked better before

--- functions.php
+++ functions.php
@@ -546,7 +546,7 @@ function cptui_register_my_cpts_lp() {
       array( 'advgb/columns', array('className' => 'site-main standalone'), array(
         array( 'advgb/column', array(), array() ),
         array( 'advgb/column', array(), array(
-          array( 'core/heading', array('level' => 2,'placeholder' => 'Classes start...' ) ),
+          array( 'core/heading', array('level' => 2,'className' => 'noMarginTop','placeholder' => 'Classes start...' ) ),
           array( 'core/paragraph', array('content' => 'Applying to Elms College is easier than you think, and there’s no fee to apply.') ),
           array( 'core/block', array('ref' => 39731) ),
         ) ),
