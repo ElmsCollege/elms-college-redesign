c7ddfd9c678429e1dec476964ff6562085f84086
millnerr <32333237+millnerr@users.noreply.github.com>
2019-11-21 10:38:16 -0500

    Now that the definition of the custom post type has been moved to functions.php, it is showing up on the commencement page - which isn't bad but is less than ideal. Trying to hide that function behind a check for $blog_id ==1 (the main section of the website). If this works it will be visible on the main site and hidden on the commencement subsite

--- functions.php
+++ functions.php
@@ -510,49 +510,52 @@ function get_first_paragraph(){
 		return $str;
 }
 
-function cptui_register_my_cpts_lp() {
-  /**
-   * Post Type: Advertising Landing Page.
-   */
-  $labels = [
-    "name" => __( "Advertising Landing Page", "gs_elms" ),
-    "singular_name" => __( "Advertising Landing Page", "gs_elms" ),
-  ];
-  $args = [
-    "label" => __( "Advertising Landing Page", "gs_elms" ),
-    "labels" => $labels,
-    "description" => "",
-    "public" => true,
-    "publicly_queryable" => true,
-    "show_ui" => true,
-    "delete_with_user" => false,
-    "show_in_rest" => true,
-    "rest_base" => "",
-    "rest_controller_class" => "WP_REST_Posts_Controller",
-    "has_archive" => false,
-    "show_in_menu" => true,
-    "show_in_nav_menus" => true,
-    "delete_with_user" => false,
-    "exclude_from_search" => true,
-    "capability_type" => "page",
-    "map_meta_cap" => true,
-    "hierarchical" => false,
-    "rewrite" => [ "slug" => "lp", "with_front" => true ],
-    "query_var" => true,
-    "menu_icon" => "dashicons-admin-page",
-    "supports" => [ "title", "editor" ],
-    'template' => array(
-      array( 'core/image', array() ),
-      array( 'advgb/columns', array('className' => 'site-main standalone'), array(
-        array( 'advgb/column', array(), array() ),
-        array( 'advgb/column', array(), array(
-          array( 'core/heading', array('level' => 2,'className' => 'noMarginTop','placeholder' => 'Classes start...' ) ),
-          array( 'core/paragraph', array('content' => 'Applying to Elms College is easier than you think, and there’s no fee to apply.') ),
-          array( 'core/block', array('ref' => 39825) ),
-        ) ),
-      ) )
-    ),
-  ];
-  register_post_type( "lp", $args );
+//we only want the advertising landing page  subsite
+if($GLOBALS["blog_id"] == 1){
+	function cptui_register_my_cpts_lp() {
+	  /**
+	   * Post Type: Advertising Landing Page.
+	   */
+	  $labels = [
+		"name" => __( "Advertising Landing Page", "gs_elms" ),
+		"singular_name" => __( "Advertising Landing Page", "gs_elms" ),
+	  ];
+	  $args = [
+		"label" => __( "Advertising Landing Page", "gs_elms" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => true,
+		"capability_type" => "page",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => [ "slug" => "lp", "with_front" => true ],
+		"query_var" => true,
+		"menu_icon" => "dashicons-admin-page",
+		"supports" => [ "title", "editor" ],
+		'template' => array(
+		  array( 'core/image', array() ),
+		  array( 'advgb/columns', array('className' => 'site-main standalone'), array(
+			array( 'advgb/column', array(), array() ),
+			array( 'advgb/column', array(), array(
+			  array( 'core/heading', array('level' => 2,'className' => 'noMarginTop','placeholder' => 'Classes start...' ) ),
+			  array( 'core/paragraph', array('content' => 'Applying to Elms College is easier than you think, and there’s no fee to apply.') ),
+			  array( 'core/block', array('ref' => 39825) ),
+			) ),
+		  ) )
+		),
+	  ];
+	  register_post_type( "lp", $args );
+	}
+	add_action( 'init', 'cptui_register_my_cpts_lp' );
 }
-add_action( 'init', 'cptui_register_my_cpts_lp' );
