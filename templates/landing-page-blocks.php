feed104484d115a3152e82a4ed0d73aecfdbab86
millnerr <32333237+millnerr@users.noreply.github.com>
2020-02-27 14:27:14 -0500

    Update landing-page-blocks.php
    Addendum to previous commit: need a z-index rule too

--- templates/landing-page-blocks.php
+++ templates/landing-page-blocks.php
@@ -19,7 +19,7 @@ while ( have_posts() ) : the_post();
 ?>
 <style>
 	#imageHeading{background-color:unset;}
-	#imageHeading img{position:absolute; top:0; left:0; width:100%; height:100%;}
+	#imageHeading img{position:absolute; top:0; left:0; width:100%; height:100%;z-index:-1;}
 </style>
 <div id="imageHeading" class="section-heading">
 	<?php
