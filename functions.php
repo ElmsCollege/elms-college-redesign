50186624b00d34a296b00d6902f281695765b30a
millnerr <32333237+millnerr@users.noreply.github.com>
2020-06-30 14:49:31 -0400

    Correct an issue with variables
    Correcting an issue that was noticed by CodeFactor during a PR check

--- functions.php
+++ functions.php
@@ -684,7 +684,7 @@ add_action('pre_get_posts', function($query){
     $result = $wpdb->get_results($test_query);
 
     // if no match, return default query, or if there's no parent post, this is not necessary
-    if (!($post = current($result)) || !$post->post_parent) {
+    if (!($post == current($result)) || !$post->post_parent) {
       return $original_query;
     }
 
