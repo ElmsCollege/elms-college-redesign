0085d2a33644f85927eea0c5f3f076f4b599b547
millnerr <32333237+millnerr@users.noreply.github.com>
2020-07-10 09:09:00 -0400

    Adding is_tree function to functions.php
    Adding the is_tree (found here: https://css-tricks.com/snippets/wordpress/if-page-is-parent-or-child/) function to provide an easy way to check if something is a specific page or has a specific parent page. This is being used in the code that controls the left rail navigation

--- functions.php
+++ functions.php
@@ -711,3 +711,11 @@ add_action('pre_get_posts', function($query){
 
 
 }, 1);
+
+function is_tree($pid) {// $pid = The ID of the page we're looking for pages underneath
+	global $post;// load details about this page
+	if(is_page()&&($post->post_parent==$pid||is_page($pid))) 
+	   return true;// we're at the page or at a sub page
+	else 
+	   return false;// we're elsewhere
+};
