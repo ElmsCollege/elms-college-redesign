83edd0e6ccd62ae6d7050d192ee8c44db87c348c
millnerr <32333237+millnerr@users.noreply.github.com>
2020-07-31 09:21:12 -0400

    Updating nav bar conversion filter
    Previous nav bar conversion filter (not:(.toggle)) didn't work because toggle is on the <a> not the <li>. Adding a new class to the <li> and using that to filter the nav bar.

--- js/functions.js
+++ js/functions.js
@@ -127,7 +127,7 @@
 
       $('<select class="opening-select" aria-label="Left rail navigation options"></select>').insertBefore($('.site-main'));
 
-      jQuery('ul.parent-sidebar-menu li:not(.toggle)').each(function (index) {
+      jQuery('ul.parent-sidebar-menu li:not(.toggle-parent)').each(function (index) {
         var parent = jQuery(jQuery(this).parent()).attr('class');
         var value = jQuery(jQuery(this).find('a').get(0)).text();
         var newOptionFromNav = jQuery('<option value="' + (index + 1) + '">' + value + '</option>');
