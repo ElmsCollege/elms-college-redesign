b4a3bf8f2bdc3cdef391394a0192ff5d16789323
millnerr <32333237+millnerr@users.noreply.github.com>
2020-08-18 15:29:22 -0400

    Removing Owl Carousel
    Removing Owl Carousl (JS file, styling rules, and the enqueing function) because I don't think we're using it.

--- functions.php
+++ functions.php
@@ -118,8 +118,6 @@ function gs_elms_scripts() {
 	wp_enqueue_script( 'gs_elms-modernizr', get_template_directory_uri() . '/js/modernizr.js', array(), '20161215', true );
 	wp_enqueue_script( 'gs_elms-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20161215', true );
 
-	wp_enqueue_script( 'gs_elms-owl-carousel-js', get_template_directory_uri() . '/js/owl.carousel.min.js', array(), '20160413', true );
-
 	wp_enqueue_script( 'gs_elms-jquery-ui', get_template_directory_uri() . '/js/jquery-ui.js', array(), '20160413', true );
 	wp_enqueue_script( 'gs_elms-velocity', get_template_directory_uri() . '/js/velocity.min.js', array(), '20151215', true );
 
