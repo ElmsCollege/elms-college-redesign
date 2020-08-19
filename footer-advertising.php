d8eeba87e997f068bb258e53dad5177efb2cf1f6
millnerr <32333237+millnerr@users.noreply.github.com>
2020-08-19 14:38:03 -0400

    Cleaning up footer classes
    Cleaning up the classes used to style the footer. Currently there are a bunch of styles that overlap/overrule each other. Trying to clean that up so the page will load faster.

--- footer-advertising.php
+++ footer-advertising.php
@@ -15,9 +15,9 @@ $address = get_field("address", "option");
 
 </div><!-- #content -->
 
-<footer id="colophon" class="site-footer pure-g" role="contentinfo">
+<footer id="colophon" class="site-footer" role="contentinfo">
 <?php the_field("advertising_tracking_pixel"); ?>
-	<div class="site-identity pure-u-1 pure-u-lg-7-24 centerText">
+	<div class="site-identity pure-u-lg-7-24 centerText">
   		<div class="site-branding footer-item-inner noMarginLeft">
   				<p class="site-title"><a href="<?php echo esc_url( real_homepage_link() ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 					<p>291 Springfield Street<br />Chicopee, MA 01013-2839<br />
