da8894c0571df1cb3e4fcd0591243ae8575b10d6
millnerr <32333237+millnerr@users.noreply.github.com>
2020-08-20 10:20:45 -0400

    Update library-interior-page.php
    The library interior page template uses a different content template (content-page.php vs program-page-content.php) than the rest of the site. This means there are minor styling differences between those pages and the program pages. Forcing the template use the standard content template (program-page-content.php) to make the pages more consistent.

--- templates/library-interior-page.php
+++ templates/library-interior-page.php
@@ -11,30 +11,6 @@ if(is_page( array( 5398, 5453, 5480, 5485, 7588) )){
 };
 
 get_template_part("template-parts/header-selector");
+get_template_part("template-parts/program-page-content");
 
-get_template_part("template-parts/page-heading");
-?>
-<div id="primary" class="content-area pure-g">
-		<main id="main" class="site-main pure-u-1 <?php echo ($has_sidebar ? "pure-u-md-7-12 pure-u-lg-2-3" : "standalone") ; ?>" role="main">
-
-			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
-    <?php if ($has_sidebar) : ?>
-        <?php get_sidebar(); ?>
-    <?php endif; ?>
-</div><!-- #primary -->
-
-<?php
 get_footer("library");
