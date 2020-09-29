a7ef1ddefb4a2ddfc35ac7fbee34f8ab59f3c012
millnerr <32333237+millnerr@users.noreply.github.com>
2020-09-29 13:21:25 -0400

    Cleaning up codefactor suggestions
    Cleaning up some of the errors that Codefactor found when I created the pull request. Some of the duplicate selector changes will be corrected as the the extended functionality of scss is explored.

--- config.rb
+++ config.rb
@@ -13,7 +13,7 @@ environment = :development
 
 require 'fileutils'
 on_stylesheet_saved do |file|
-  if File.exists?(file) && File.basename(file) == "style.css"
+  if File.exist?(file) && File.basename(file) == "style.css"
     puts "Moving: #{file}"
     FileUtils.mv(file, File.dirname(file) + "/../" + File.basename(file))
   end
