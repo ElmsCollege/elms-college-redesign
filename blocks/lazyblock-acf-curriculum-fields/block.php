5cf6c2b6fb16d65c3c6b53af409f0c516805d8f5
millnerr <32333237+millnerr@users.noreply.github.com>
2021-01-28 11:05:49 -0500

    Changing the ACF Curriculum Block code from being in the editor to being in the repository. This will allow Ryan to be more confident making changes because the code will be under version control. This is a direct copy from the site to be used as a baseline before making changes

---
+++ blocks/lazyblock-acf-curriculum-fields/block.php
@@ -0,0 +1,36 @@
+<?php
+	#controls the ACF curriculum block
+?>
+<div id="courses" class="information-block">
+  <h2 class="greenBGwhiteText noMargins">Curriculum</h2>
+  <div class="inner-padding">
+  <?php
+    $rows = get_field('curriculum');
+    if($rows){
+      foreach($rows as $row){
+        $courseID = preg_replace('/[^a-zA-Z0-9]+/', '', $row['curriculum_program_name']);
+        //echo $courseID;
+        echo "<div id='" . $courseID . "' class='collapseomatic noarrow'><h4 class='noMarginTop'>Click to view course requirements for the " . $row['curriculum_program_name'] . (($row['curriculum_major_or_minor']=='minor')?' minor':"");
+        echo ".</h4></div><div id='swap-" . $courseID . "' class='collapseomatic noarrow' style='display: none;'><h4 class='noMarginTop'>Click to collapse the course requirements for the " . $row['curriculum_program_name'] .(($row['curriculum_major_or_minor']=='minor')?' minor':"");
+        echo ".</h4></div><div class='collapseomatic_content' id='target-" . $courseID . "'>";
+        echo $row['curriculum_text'];
+        
+        $tables = $row['course_requirements_table_name'];
+        if($tables){
+          foreach($tables as $table){
+            echo "<h4 class='noMarginBottom noMarginTop'>" . $table['course_list_table_label'] ."</h4><table><thead><tr><th>Course #</th><th>Formerly</th><th>Course Name</th><th># of Credit Hours</th></tr></thead><tbody>";
+            $courses = $table['course-list-repeater'];
+            if($courses){
+              foreach($courses as $course){
+                echo "<tr><td>" . $course['course_number'] . "</td><td>" . $course['old_course_number'] . "</td><td>" . $course['course_name'] . "</td><td>" . $course['number_of_credits'] ."</td></tr>";
+              }
+            echo "</tbody></table>";
+            }
+          }
+        }
+        echo "</div>";
+      }
+    }
+  ?>
+  </div>
+</div>
