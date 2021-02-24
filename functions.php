dfa600ed75a3d0ce4673537de131933e5f8a9cea
millnerr <32333237+millnerr@users.noreply.github.com>
2021-02-24 10:43:24 -0500

    Pulling the disable emojis scripts out of the theme and turning them into a plugin. This code will appear in the plugin repository in the disable-emojis folder

--- functions.php
+++ functions.php
@@ -360,34 +360,6 @@ function hide_domain_in_permalink_on_admin () {
     return $str;
   }
 
-  /**
-   * Disable the emoji's
-   */
-  function disable_emojis() {
-    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-    remove_action( 'wp_print_styles', 'print_emoji_styles' );
-    remove_action( 'admin_print_styles', 'print_emoji_styles' );
-    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-
-    // Remove from TinyMCE
-    add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
-  }
-  add_action( 'init', 'disable_emojis' );
-
-  /**
-   * Filter out the tinymce emoji plugin.
-   */
-  function disable_emojis_tinymce( $plugins ) {
-    if ( is_array( $plugins ) ) {
-      return array_diff( $plugins, array( 'wpemoji' ) );
-    } else {
-      return array();
-    }
-  }
-
   add_action( 'registered_post_type', 'make_posts_hierarchical', 10, 2 );
 
   // Runs after each post type is registered
