6ea6fe269039e5845ef989fa148d7e30e9d59dbc
millnerr <32333237+millnerr@users.noreply.github.com>
2021-03-09 15:39:07 -0500

    The main nursing landing page needs another custom block. Initial commmit of the two-column-cutout block. This is a straight copy from the template and will be customized in future commits

---
+++ blocks/lazyblock-two-column-cutout/block.php
@@ -0,0 +1,8 @@
+<div class="short-content">
+	<div class="field-background" style="background-image: url('<?php echo $attribute['background']['url']; ?>')">&nbsp;</div>
+	<div class="short-content-inner">
+		<div class="short">
+			<?php echo $attribute['blocks']; ?>
+		</div>
+	</div>
+</div>
