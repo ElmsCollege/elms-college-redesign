8cb56cc40b23edc7de10a772b0c3e736dccc6222
millnerr <32333237+millnerr@users.noreply.github.com>
2021-03-09 15:57:56 -0500

    Lazyblocks uses a plural $attributes, not a singular. Fixing that so the block shows up

--- blocks/lazyblock-two-column-cutout/block.php
+++ blocks/lazyblock-two-column-cutout/block.php
@@ -1,8 +1,8 @@
 <div class="short-content">
-	<div class="field-background" style="background-image: url('<?php echo $attribute['background']['url']; ?>')">&nbsp;</div>
+	<div class="field-background" style="background-image: url('<?php echo $attributes['background']['url']; ?>')">&nbsp;</div>
 	<div class="short-content-inner">
 		<div class="short">
-			<?php echo $attribute['blocks']; ?>
+			<?php echo $attributes['blocks']; ?>
 		</div>
 	</div>
 </div>
